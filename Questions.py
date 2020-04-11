e20840343fbd264208a2e53c36224b6f9d4ffbd8
Angel Retana <40252881+AERM2018@users.noreply.github.com>
2020-04-11 18:09:47 -0500

    Update Questions.py

--- Questions.py
+++ Questions.py
@@ -33,11 +33,11 @@ Questions = {
     },
     5:{
         "text":"What was the first film created by \"Disney\" ",
-        "ans1":"respuesta1",
-        "ans2":"respuesta2",
-        "ans3":"respuesta3",
-        "ans4":"respuesta4",
-        "correct":"respuesta2"
+        "a":"Snow White and the Seven Dwarfs",
+        "b":"Frozen",
+        "c":"The Princess and the Frog",
+        "d":"Toy Story",
+        "correct":"a"
     },
     6:{
         "text":"What is the largest planet in the solar system?",
@@ -56,38 +56,197 @@ Questions = {
         "correct":"c"
     },
     8:{
-        "q":"Hola soy la segunda pregunta 8",
-        "ans1":"respuesta1",
-        "ans2":"respuesta2",
-        "ans3":"respuesta3",
-        "ans4":"respuesta4",
-        "correct":"respuesta2"
+        "text":"How many bones has the human body?",
+        "a":"200",
+        "b":"306",
+        "c":"310",
+        "d":"206",
+        "correct":"d"
     },
     9:{
-        "q":"Hola soy la segunda pregunta 9",
-        "ans1":"respuesta1",
-        "ans2":"respuesta2",
-        "ans3":"respuesta3",
-        "ans4":"respuesta4",
-        "correct":"respuesta2"
+        "text":"Where did the Olympics originate?",
+        "a":"Rome",
+        "b":"Mexico",
+        "c":"Greece",
+        "d":"Japan",
+        "correct":"c"
     },
     10:{
-        "q":"Hola soy la segunda pregunta 10",
-        "ans1":"respuesta1",
-        "ans2":"respuesta2",
-        "ans3":"respuesta3",
-        "ans4":"respuesta4",
-        "correct":"respuesta2"
+        "text":"Where is the Eiffel Tower located?",
+        "a":"Paris, Frnace",
+        "b":"Madrid, Spain",
+        "c":"Moscow, Russia",
+        "d":"Venice, Italy",
+        "correct":"a"
     },
     11:{
-        "q":"Hola soy la segunda pregunta 11",
-        "ans1":"respuesta1",
-        "ans2":"respuesta2",
-        "ans3":"respuesta3",
-        "ans4":"respuesta4",
-        "correct":"respuesta2"
+        "text":"How many languages are spoken in Mexico?",
+        "a":"Only spanish",
+        "b":"68",
+        "c":"15",
+        "d":"22",
+        "correct":"b"
+    },
+    12:{
+        "text":"If 50 is the 100%, what is the 90%",
+        "a":"40",
+        "b":"42",
+        "c":"50",
+        "d":"45",
+        "correct":"d"
+    },
+    13:{
+        "text":"What is the name of the science that studies the plants?",
+        "a":"Botany",
+        "b":"Chemestry",
+        "c":"Biology",
+        "d":"Mathematics",
+        "correct":"a"
+    },
+    14:{
+        "text":"What was the name of hell in the indigenous culture of mexico?",
+        "a":"Mitlan",
+        "b":"Chametla",
+        "c":"Inframundo",
+        "d":"None of the above",
+        "correct":"b"
+    },
+    15:{
+        "text":"What is the chemical formula of water?",
+        "a":"HSO4",
+        "b":"H2O2",
+        "c":"H2O",
+        "d":"HCl",
+        "correct":"c"
+    },
+    16:{
+        "text":"",
+        "a":"",
+        "b":"",
+        "c":"",
+        "d":"",
+        "correct":""
+    },
+    17:{
+        "text":"",
+        "a":"",
+        "b":"",
+        "c":"",
+        "d":"",
+        "correct":""
+    },
+    18:{
+        "text":"",
+        "a":"",
+        "b":"",
+        "c":"",
+        "d":"",
+        "correct":""
+    },
+    18:{
+        "text":"",
+        "a":"",
+        "b":"",
+        "c":"",
+        "d":"",
+        "correct":""
+    },
+    19:{
+        "text":"",
+        "a":"",
+        "b":"",
+        "c":"",
+        "d":"",
+        "correct":""
+    },
+    20:{
+        "text":"",
+        "a":"",
+        "b":"",
+        "c":"",
+        "d":"",
+        "correct":""
+    },
+    21:{
+        "text":"",
+        "a":"",
+        "b":"",
+        "c":"",
+        "d":"",
+        "correct":""
+    },
+    22:{
+        "text":"",
+        "a":"",
+        "b":"",
+        "c":"",
+        "d":"",
+        "correct":""
+    },
+    23:{
+        "text":"",
+        "a":"",
+        "b":"",
+        "c":"",
+        "d":"",
+        "correct":""
+    },
+    24:{
+        "text":"",
+        "a":"",
+        "b":"",
+        "c":"",
+        "d":"",
+        "correct":""
+    },
+    25:{
+        "text":"",
+        "a":"",
+        "b":"",
+        "c":"",
+        "d":"",
+        "correct":""
+    },
+    26:{
+        "text":"",
+        "a":"",
+        "b":"",
+        "c":"",
+        "d":"",
+        "correct":""
+    },
+    27:{
+        "text":"",
+        "a":"",
+        "b":"",
+        "c":"",
+        "d":"",
+        "correct":""
+    },
+    28:{
+        "text":"",
+        "a":"",
+        "b":"",
+        "c":"",
+        "d":"",
+        "correct":""
+    },
+    29:{
+        "text":"",
+        "a":"",
+        "b":"",
+        "c":"",
+        "d":"",
+        "correct":""
+    },
+    30:{
+        "text":"",
+        "a":"",
+        "b":"",
+        "c":"",
+        "d":"",
+        "correct":""
     }
 }
 
 
-# for i in Questions.keys():
